a09e205f1aac01baf590f649349dd814b674b033
ngonyani1014 <waziringonyani9401@gmail.com>
2026-09-23 11:15:56 +0300

    Implement MathJax integration for dynamic rendering
    Added dynamic loading of MathJax with polyfill support.

--- notes/physics_form2.js
+++ notes/physics_form2.js
@@ -1,1 +1,26 @@
+// script.js - Dynamic MathJax Integration
 
+(function loadMathJax() {
+    // 1. Configure MathJax before loading
+    window.MathJax = {
+        tex: {
+            inlineMath: [['$', '$'], ['\\(', '\\)']],
+            displayMath: [['$$', '$$'], ['\\[', '\\]']]
+        },
+        svg: {
+            fontCache: 'global'
+        }
+    };
+
+    // 2. Load Polyfill script
+    var polyfill = document.createElement('script');
+    polyfill.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
+    document.head.appendChild(polyfill);
+
+    // 3. Load MathJax v3 script asynchronously
+    var mathjax = document.createElement('script');
+    mathjax.id = 'MathJax-script';
+    mathjax.async = true;
+    mathjax.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+    document.head.appendChild(mathjax);
+})();
